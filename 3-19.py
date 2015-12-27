c585246993178f3aa9451a512f78249d86b56f8d
Andrei Vacariu <andrei@avacariu.me>
2015-12-27 12:28:41 -0800

    Add challenge 19

---
+++ 3-19.py
@@ -0,0 +1,77 @@
+from lib.crypto import aes_ctr, random_key
+from lib.utils import xor
+import base64
+import string
+
+plaintexts = [
+    "SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==",
+    "Q29taW5nIHdpdGggdml2aWQgZmFjZXM=",
+    "RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==",
+    "RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=",
+    "SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk",
+    "T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+    "T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=",
+    "UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+    "QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=",
+    "T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl",
+    "VG8gcGxlYXNlIGEgY29tcGFuaW9u",
+    "QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==",
+    "QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=",
+    "QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==",
+    "QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=",
+    "QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
+    "VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==",
+    "SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==",
+    "SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==",
+    "VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==",
+    "V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==",
+    "V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==",
+    "U2hlIHJvZGUgdG8gaGFycmllcnM/",
+    "VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=",
+    "QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=",
+    "VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=",
+    "V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=",
+    "SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==",
+    "U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==",
+    "U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=",
+    "VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==",
+    "QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu",
+    "SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=",
+    "VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs",
+    "WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=",
+    "SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0",
+    "SW4gdGhlIGNhc3VhbCBjb21lZHk7",
+    "SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=",
+    "VHJhbnNmb3JtZWQgdXR0ZXJseTo=",
+    "QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
+]
+
+key = random_key()
+nonce = 0
+
+alphabet = string.ascii_letters + string.digits + ".,:'\" "
+
+ciphertexts = [aes_ctr(key, base64.b64decode(pt), nonce) for pt in plaintexts]
+
+long_ct, *other_cts = sorted(ciphertexts, key=len, reverse=True)
+
+def check(guess_pt: bytes):
+    stretched_pt = bytes(guess_pt + b"\x00"*(len(long_ct) - len(guess_pt)))
+    key = xor(long_ct, stretched_pt)
+
+    for ct in other_cts:
+        print(xor(ct, key)[:len(guess_pt)].decode())
+
+def go(guess: bytes):
+    for c in alphabet:
+        print((guess + c.encode()).decode())
+        check(guess + c.encode())
+        input()
+
+# plantext was guessed by using the go function to guess a letter and print the
+# decrypted versions of all the other ciphertexts. Once they all seemed to make
+# sense, keep the letter and guess a new one.
+long_pt = b"He, too, has been changed in his turn"
+key_stream = xor(long_pt, long_ct)
+for ct in ciphertexts:
+    print(xor(key_stream, ct).decode())
